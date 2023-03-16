d8ea1088fa9d716108fd83de17371bedda936fdf
guialvesds <guilherme_ealves@outlook.com>
2023-03-15 22:31:30 -0300

    inicial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import connectDb from "./src/database/db.js";
+
+dotenv.config();
+
+const corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200
+};
+
+const app = express();
+
+connectDb();
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.use('/', (req, res) => {
+ return res.send('Bem vindo(a) a Api Diário Web 1.0.')
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor conectado na porta: ${PORT}`);
+})
---
+++ src/database/db.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+mongoose.Promise = global.Promise;
+
+mongoose.set('strictQuery', true)
+
+const connectDb = () => {
+  console.log("Aguarde, conectando ao banco de dados...");
+
+  mongoose
+    .connect(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@diarioweb.6smc7se.mongodb.net/?retryWrites=true&w=majority`,
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    )
+    .then(() => {
+      console.log("Conectado ao banco de dados com sucesso!");
+    })
+    .catch((error) =>
+      console.log("Não foi possível conectar ao banco de dados ", error)
+    );
+};
+
+
+
+export default connectDb;
